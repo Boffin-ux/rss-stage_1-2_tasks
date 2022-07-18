ecec13239000050a1fa415ef26cc963e2343e4a9
Boffin-ux <fnamily@mail.ru>
2022-07-18 09:34:35 +0300

    feat: add event handlers

--- Online-store/src/controllers/AppControler.ts
+++ Online-store/src/controllers/AppControler.ts
@@ -35,7 +35,13 @@ export default class AppControler {
 
     countOrders() {
         const getCount = <HTMLElement>document.querySelector('.cart__counter');
-        getCount.textContent = this.storage.getOrders().length.toString();
+        if (this.storage.getOrders() && this.storage.getOrders().length > 0) {
+            getCount.classList.add('counter--active');
+            getCount.textContent = this.storage.getOrders().length.toString();
+        } else if (!this.storage.getOrders() || this.storage.getOrders().length < 1) {
+            getCount.classList.remove('counter--active');
+            getCount.textContent = '0';
+        }
     }
 
     eventRange(arrValue: Values) {
@@ -98,6 +104,166 @@ export default class AppControler {
         });
     }
 
+    eventSearch() {
+        const searchInput = <HTMLInputElement>document.querySelector('.search-form__input');
+        const clearFormBtn = <HTMLElement>document.querySelector('.search-form__clear');
+        const objKey = 'search';
+
+        const getFilter = this.storage.getFilter();
+        const valueStorage = getFilter.search?.toString();
+
+        if (valueStorage) {
+            searchInput.value = valueStorage;
+            clearFormBtn.classList.add('clear--active');
+        } else {
+            searchInput.value = '';
+            clearFormBtn.classList.remove('clear--active');
+        }
+
+        searchInput.focus();
+        searchInput.addEventListener('input', () => {
+            this.storage.setFilter(objKey, searchInput.value);
+            if (searchInput.value.length === 0) {
+                clearFormBtn.classList.remove('clear--active');
+            } else {
+                clearFormBtn.classList.add('clear--active');
+            }
+            void this.updateView();
+        });
+        clearFormBtn.addEventListener('click', () => {
+            searchInput.value = '';
+            this.storage.setFilter(objKey, searchInput.value);
+            clearFormBtn.classList.remove('clear--active');
+            void this.updateView();
+        });
+    }
+
+    eventPopular() {
+        const checkbox = <HTMLInputElement>document.querySelector('.popular__checkbox');
+
+        const activeBox = this.storage.getFilter();
+        const objKey = 'popular';
+        const objGetValue = activeBox[objKey];
+        if (objGetValue && objGetValue[0]) checkbox.checked = true;
+
+        checkbox.addEventListener('click', () => {
+            if (checkbox.checked) {
+                this.storage.delElFilter(objKey, false);
+                this.storage.setFilter(objKey, true);
+            } else {
+                this.storage.delElFilter(objKey, true);
+                this.storage.setFilter(objKey, false);
+            }
+            void this.updateView();
+        });
+    }
+
+    eventSort() {
+        const list = <HTMLElement>document.querySelector('.sort__dropdown > .dropdown__list');
+        const dropdownItem = document.querySelectorAll('.dropdown__item');
+        const activeSort = this.storage.getSort();
+
+        if (activeSort && activeSort.length > 0) {
+            list.classList.remove('list--hidden');
+            [...dropdownItem].forEach((item) => {
+                if (item instanceof HTMLElement && item.dataset.sort === activeSort) {
+                    item.classList.add('item--active');
+                }
+            });
+        }
+
+        list.addEventListener('click', (e) => {
+            const target = <HTMLElement>e.target;
+
+            if (target.closest('.dropdown__item')) {
+                [...dropdownItem].forEach((item) => {
+                    item.classList.remove('item--active');
+                });
+                target.classList.add('item--active');
+                if (target.dataset.sort === 'a-z') {
+                    this.storage.setSort('a-z');
+                } else if (target.dataset.sort === 'z-a') {
+                    this.storage.setSort('z-a');
+                } else if (target.dataset.sort === 'price-inc') {
+                    this.storage.setSort('price-inc');
+                } else if (target.dataset.sort === 'price-dic') {
+                    this.storage.setSort('price-dic');
+                }
+                void this.updateView();
+            }
+        });
+    }
+
+    eventCheckBox(keys: string[]) {
+        const setListener = (selector: string) => {
+            const list = <HTMLElement>document.querySelector(`.${selector}__dropdown > .dropdown__list`);
+            const itemsInput = list.querySelectorAll('.dropdown__label input');
+            const items = list.querySelectorAll('.dropdown__label');
+            const activeDropdown = this.storage.getFilter();
+            const objKey = selector;
+            const objGetValue = activeDropdown[objKey as keyof typeof activeDropdown] as string[];
+
+            if (objGetValue && objGetValue.length > 0) {
+                list.classList.remove('list--hidden');
+                items.forEach((item) => {
+                    const title = item.querySelector('.dropdown__item-title')?.textContent?.toString();
+                    const checkbox = <HTMLInputElement>item.querySelector('.dropdown__checkbox');
+                    if (title && objGetValue.includes(title.toString())) {
+                        checkbox.checked = true;
+                    }
+                });
+            }
+
+            itemsInput.forEach((input) => input.addEventListener('click', (e) => e.stopPropagation()));
+
+            items.forEach((item) => {
+                item.addEventListener('click', () => {
+                    const title = item.querySelector('.dropdown__item-title')?.textContent?.toString();
+                    const checkbox = <HTMLInputElement>item.querySelector('.dropdown__checkbox');
+                    if (title && checkbox.checked === false) {
+                        this.storage.setFilter(objKey, title);
+                    } else if (title && checkbox.checked === true) {
+                        this.storage.delElFilter(objKey, title);
+                    }
+                    void this.updateView();
+                });
+            });
+        };
+
+        keys.map((key) => setListener(key));
+    }
+
+    resetSetings(arrValue: Values) {
+        const clearFilterBtn = document.getElementById('clear-filter');
+        const clearStorageBtn = document.getElementById('clear-storage');
+
+        clearFilterBtn?.addEventListener('click', () => {
+            const checkboxes: NodeListOf<HTMLInputElement> = document.querySelectorAll('input[type=checkbox]:checked');
+            const list: NodeListOf<HTMLElement> = document.querySelectorAll('.dropdown__list');
+            const sliderPrice = (<HTMLElement>document.getElementById('slider-price')) as noUiSlider.target;
+            sliderPrice.noUiSlider?.set([arrValue.valueMin, arrValue.valueMax]);
+
+            list.forEach((item) => {
+                const parent = <HTMLElement>item.parentNode;
+                if (parent.className.indexOf('brand')) item.classList.add('list--hidden');
+            });
+            [...checkboxes].map((checkbox) => {
+                if (checkbox && checkbox.checked) checkbox.checked = false;
+            });
+            this.storage.resetFilter();
+            this.storage = new LocalStorage();
+            void this.updateView();
+            this.eventSearch();
+        });
+
+        clearStorageBtn?.addEventListener('click', () => {
+            this.storage.resetAll();
+            this.storage = new LocalStorage();
+            document.body.innerHTML = '';
+            void this.start();
+        });
+    }
+
     async start() {
         const model = new AppModel(this.state, this.storage);
         const data = await model.getData();
@@ -112,7 +278,12 @@ export default class AppControler {
 
         this.dropdownToggle();
         this.countOrders();
+        this.eventSort();
+        this.eventSearch();
+        this.eventPopular();
         this.eventRange(getRangeSum);
+        this.eventCheckBox(Object.keys(model.getSpec(data)));
+        this.resetSetings(getRangeSum);
     }
 
     async updateView() {
--- Online-store/src/views/cards/Cards.ts
+++ Online-store/src/views/cards/Cards.ts
@@ -1,11 +1,29 @@
 import { ApiData } from '../../types/options';
+import LocalStorage from '../../controllers/LocalStorage';
 import './cards.scss';
 import '../../assets/img/default.jpg';
 
 export default class Cards {
+    storage: LocalStorage;
+
+    constructor() {
+        this.storage = new LocalStorage();
+    }
+
     render(data: Readonly<ApiData[]>) {
         const wrapCards = <HTMLElement>document.querySelector('.product__list');
         wrapCards.textContent = '';
+
+        const getFilter = this.storage.getFilter();
+        const valueStorage = getFilter.search?.toString() || '';
+        const searchItems = data.filter((item) => item.title.toLowerCase().includes(valueStorage.toLowerCase()));
+
+        if (searchItems.length < 1 || data.length < 1) {
+            const result = document.createElement('div');
+            result.classList.add('no-results');
+            result.innerHTML = `<h2>Cовпадений не найдено</h2>`;
+            wrapCards.append(result);
+        }
 
         data.forEach((item) => {
             const { id, title, imageUrl, price, specification, color } = item;
@@ -26,14 +44,32 @@ export default class Cards {
             cardImg.style.backgroundImage = `url(${imageUrl || '../../assets/img/default.jpg'})`;
 
             const cardBtn = <HTMLElement>createCard.querySelector('.card__btn');
+            this.storage.getOrders().map((card: string) => {
+                if (card === id.toString()) {
+                    createCard.classList.add('card--add');
+                    cardBtn.textContent = 'В корзине';
+                }
+            });
 
             createCard.addEventListener('click', () => {
+                const getCount = <HTMLElement>document.querySelector('.cart__counter');
+
                 if (!createCard.closest('.card--add')) {
                     createCard.classList.add('card--add');
                     cardBtn.textContent = 'В корзине';
+                    this.storage.addOrder(id.toString());
                 } else {
                     createCard.classList.remove('card--add');
                     cardBtn.textContent = 'В корзину';
+                    this.storage.setOrders(this.storage.getOrders().filter((card: string) => card !== id.toString()));
+                }
+
+                if (this.storage.getOrders() && this.storage.getOrders().length > 0) {
+                    getCount.classList.add('counter--active');
+                    getCount.textContent = this.storage.getOrders().length.toString();
+                } else if (!this.storage.getOrders() || this.storage.getOrders().length < 1) {
+                    getCount.classList.remove('counter--active');
+                    getCount.textContent = '0';
                 }
             });
         });
